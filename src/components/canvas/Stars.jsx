70d8bdcc98e8e97427a6c8e5457608be0ad8fcd5
Chaitanya <chaitanyaasole@gmail.com>
2024-10-31 15:18:20 +0530

    completed the experiment

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -19,8 +19,8 @@ const Stars = (props) => {
       <Points ref={ref} positions={sphere} stride={3} {...props}>
         <PointMaterial
           transparent
-          color="#f272c8"
-          size={0.002}
+          color="purple"
+          size={0.02}
           sizeAttenuation={true}
           depthWrite={false}
         />
@@ -31,7 +31,7 @@ const Stars = (props) => {
 
 const StarsCanvas = () => {
   return (
-    <div className="w-full h-auto absolute inset-0 z-[-1]">
+    <div className="w-full h-auto absolute inset-0 z-[-1] bg-black">
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
